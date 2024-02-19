0026084d6d4177e647d951b70cbc1ccb3606efbb
OverRevvv <rdude1612@gmail.com>
2024-02-19 21:06:14 +0530

    cleaned the server resrouce from the client repo

--- server/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'work in progress, this endpoint will serve the client app';
-  }
-}
--- server/src/items/dto/updateItemDTO.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateItemDto } from './createItemDTO';
-
-export class UpdateItemDto extends PartialType(CreateItemDto) {}
